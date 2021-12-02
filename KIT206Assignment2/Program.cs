e8d0d7c51a51949c7db2db1906a6dc8df5db007d
AdamLordUtas <94874480+AdamLordUtas@users.noreply.github.com>
2021-12-02 13:16:00 +1100

    2/12/2021

--- KIT206Assignment2/Program.cs
+++ KIT206Assignment2/Program.cs
@@ -10,7 +10,33 @@ namespace KIT206Assignment2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-Start-");
+            Console.WriteLine("-Start-\n");
+
+            List<Position> dummyPositions = new List<Position>() 
+            {
+                new Position{
+                    id = 500, 
+                    level = Level.C, 
+                    start = new DateTime(1999, 8, 21), 
+                    end = new DateTime(2001, 8, 13) }
+            };
+
+            List<Researcher> dummyResearchers = new List<Researcher>()
+            {
+                new Researcher{ 
+                    id = 500, 
+                    givenName = "John", 
+                    familyName = "Johnson", 
+                    title = "Mr", unit = "Maths", 
+                    campus = Campus.Hobart, 
+                    degree = "Maths", 
+                    email = "jjohnson@email.com", 
+                    photo = "image.jpg", 
+                    position = dummyPositions[0], 
+                    supervisorId = 100}
+            };
+
+            Console.WriteLine(dummyResearchers[0].jobTitle());
         }
     }
 }
