ee9357429bc836332111e1ef1507d226425bee52
AdamLordUtas <94874480+AdamLordUtas@users.noreply.github.com>
2021-12-08 10:22:11 +1100

    Updated 10:22am 8/12/2021

--- KIT206Assignment2/Program.cs
+++ KIT206Assignment2/Program.cs
@@ -21,57 +21,25 @@ namespace KIT206Assignment2
             erdAdapter adapter = new erdAdapter();
 
             Console.WriteLine("-Start-\n");
-
-            //Testing
-            List<Position> dummyPositions = new List<Position>() 
-            {
-                new Position{
-                    id = 500, 
-                    level = Level.C, 
-                    start = new DateTime(1999, 8, 21), 
-                    end = new DateTime(2001, 8, 13) }
-            };
-
-            //Testing
-            List<Researcher> dummyResearchers = new List<Researcher>()
-            {
-                new Researcher{ 
-                    id = 500, 
-                    givenName = "John", familyName = "Johnson",
-                    title = "Mr", unit = "Maths", 
-                    campus = Campus.Hobart, degree = "Maths", 
-                    email = "jjohnson@email.com", photo = "image.jpg", 
-                    position = dummyPositions[0], supervisorId = 100}
-            };
-
-            //Testing
-            List<Publication> dummyPublications = new List<Publication>()
-            {
-                new Publication{
-                    doi = "55.2000xp", title = "Real Journal about something",
-                    authours = "John Johnson", year = 2009,
-                    type = Ptype.Journal, citeAs = "real paper, 2009",
-                    available = new DateTime(2021, 4, 21) }
-            };
-
-            //Console.WriteLine(dummyResearchers[0].givenName +"'s position is " + dummyResearchers[0].JobTitle());
-            //Console.WriteLine("The age of " + dummyPublications[0].title + " is " + dummyPublications[0].Age() + " years old");
+            
+            //Loading Researchers
             List<Researcher>found = adapter.GetBasicResearcherDetails();
             foreach (Researcher item in found)
 	        {
                 Console.WriteLine(item.listDisplay());
 	        }
 
-            //List<Publication> foundPub = adapter.GetBasicPublicationDetails(found[0]);
-
-            //foreach (var item in foundPub)
-            //{
-              //  Console.WriteLine(item.title);
-            //}
-
+            //Getting a specific researcher's details
             Researcher specificResearcher = adapter.GetFullResearcherDetails(found[6]);
+            Console.WriteLine("\n" + specificResearcher.listDisplay() + "\n");
 
-            Console.WriteLine("\n" + specificResearcher.listDisplay());
+            //Loading Publications for first person
+            List<Publication> foundPub = adapter.GetBasicPublicationDetails(found[0]);
+            foreach (var item in foundPub)
+            {
+                Console.WriteLine(item.title);
+            }
+
         }
     }
 }
