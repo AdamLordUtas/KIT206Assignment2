d08c6db7f5d3992693d063a4f29537d7621789c1
simranc0 <simranc0@utas.edu.au>
2021-12-20 19:16:10 +1100

    Up

--- KIT206Assignment2/Research/Publication.cs
+++ KIT206Assignment2/Research/Publication.cs
@@ -7,17 +7,22 @@ using System.Threading.Tasks;
 namespace KIT206Assignment2.Research
 {
     //Enum for types of publications
-    public enum Ptype { Conference, Journal, Other}
+    public enum Ptype 
+    { 
+        Conference, 
+        Journal, 
+        Other
+    }
 
     class Publication
     {
-        public string doi { get; set; }
-        public string title { get; set; }
-        public string authours { get; set; }
-        public int year { get; set; }
-        public Ptype type { get; set; }
-        public string citeAs { get; set; }
-        public DateTime available { get; set; }
+        public string doi { get; set; }//uniquely identifiable doi
+        public string title { get; set; }//publication title
+        public string authours { get; set; }//shows author
+        public int year { get; set; }//year of publication
+        public Ptype type { get; set; }//one of the Conference, Journal or Other
+        public string citeAs { get; set; }//shows the details from database
+        public DateTime available { get; set; }//the date when the publication was first available
 
         //Number of years the since the first publication year
         public int Age() 
