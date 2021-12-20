52199305a597c4e1e8c0f5c37738061927951d97
AdamLordUtas <94874480+AdamLordUtas@users.noreply.github.com>
2021-12-20 12:40:02 +1100

    Updated 20/12/2021

--- KIT206Assignment2/Database/erdAdapter.cs
+++ KIT206Assignment2/Database/erdAdapter.cs
@@ -367,21 +367,22 @@ namespace KIT206Assignment2.Database
 				MySqlCommand cmd = new MySqlCommand(String.Format("select publication.doi, publication.available from researcher_publication join publication on researcher_publication.doi = publication.doi where researcher_publication.researcher_id = {0} and publication.available >= {1})", researcherId, threeYears.ToString("yyyy/MM/dd")), conn);
 				rdr = cmd.ExecuteReader();
 
-				while (rdr.Read())
-				{
-					Console.WriteLine(rdr.GetString(0) + " " + rdr.GetDateTime(1));
-					count++;
-				}
+					while (rdr.Read())
+					{
+						Console.WriteLine(rdr.GetString(0) + " " + rdr.GetDateTime(1));
+						count++;
+					}
 
-			}
+				}
 			catch (Exception e)
 			{
 				Console.WriteLine("Something went wrong " + e);
 			}
+			
 			finally
 			{
 				// close the reader
-				if (rdr != null)
+				if(rdr != null)
 				{
 					rdr.Close();
 				}
@@ -395,8 +396,6 @@ namespace KIT206Assignment2.Database
 
 			return count;
 		}
-
-
-		}
 	}
+}
 
